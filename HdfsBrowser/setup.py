ee1b679ef996ff8c2f2ad0d5540319a086b581e0
SWAN GitHab <Omar.Zapata@cern.ch>
2020-07-13 13:08:37 +0000

    created tag for package HdfsBrowser with version 0.0.10

--- HdfsBrowser/setup.py
+++ HdfsBrowser/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hdfsbrowser',
-      version='0.0.9',
+      version='0.0.10',
       description='HDFS Browser Extension for Jupyter Notebook',
       author='Prasanth Kothuri',
       author_email='prasanth_kothuri@hotmail.com',
