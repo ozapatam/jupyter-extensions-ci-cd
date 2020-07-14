e5ad93852a385a7139734435fa9c8bdfeec7f7c6
Omar Zapata <Omar.Zapata@cern.ch>
2020-07-14 11:58:43 +0200

    SWAN Bump, updating package HdfsBrowser with version 1.2.11

--- HdfsBrowser/setup.py
+++ HdfsBrowser/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hdfsbrowser',
-      version='1.2.10',
+      version='1.2.11',
       description='HDFS Browser Extension for Jupyter Notebook',
       author='Prasanth Kothuri',
       author_email='prasanth_kothuri@hotmail.com',
